3bb0bb1181a1a3685b12088e37fb81bf4ff4d56a
AwPeRNooB <awperkumar@gmail.com>
2025-02-22 23:44:24 +0530

    Add files via upload

---
+++ src/components/AboutUs.js
@@ -0,0 +1,20 @@
+function AboutUs() 
+{
+return(<>
+<div className='content'>
+          <h2>Welcome to Auction App</h2>
+          <p>
+            We here help people get their items sold through verified buyers and real time price.
+
+          </p>
+          </div>
+          <ul>Features of the Website </ul>
+          <li>User Authentication</li>
+          <li>Sign In and Sign Up </li>
+          <li>Real time Bidding </li>
+          <li>Transperent Bidding Process</li>
+          </>
+)
+}
+
+export default AboutUs
